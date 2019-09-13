ff56628e7245fb91804228ecfdcaa6ce60dd6320
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-09-13 11:30:14 +0200

    Test Calculator avec les boutons

--- calculator.php
+++ calculator.php
@@ -15,9 +15,12 @@ function end_page()
     echo '<hr/><br/><strong>Calculator</strong><br/><hr/>';
 
     echo '<form action="action.php" method="post">
-          <p>Votre nom : <input type="text" name="nom" /></p>
-          <p>Votre âge : <input type="text" name="age" /></p>
-          <p><input type="submit" value="OK"></p>
+          <p> op1 <input type="text" name="nom" /></p>
+          <p> op2 <input type="text" name="age" /></p>
+          <p><input checked ="checked" type="radio" name="op" value="*"/>*<br/></p>
+          <p><input type="radio" name="op" value="+"/>+<br/></p>
+          <p><input type="radio" name="op" value="-"/>-<br/></p>
+          <p><input type="radio" name="op" value="/"/>/<br/></p>
           </form>';
 
     echo '</body>
