8f05aa29a6af0e9ecdeaa023212c76a997b5ccc4
POUZARGUES Jeremy <p18003049@A-F003291.salsa.univ-amu.fr>
2019-09-13 09:50:54 +0200

    premier essai

---
+++ td_1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang = 'fr'>
+
+  <head>
+        <title> TD1 </title>
+  </head>
+
+  <body>
+
+    <?php
+        $var1 = 6;
+        $var2 = 1.3;
+        $var3 = 'Variable3';
+    ?>
+
+    <?php
+        echo "$var1 + $var2";
+    ?>
+
+    <?php
+        echo $var1 + $var2;
+    ?>
+
+    <?php
+        echo $var1 + $var3;
+    ?>
+
+
+    <?php
+        echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+        echo 'C\'est pas mal non ? <br/>';
+    ?>
+  </body>
+
+
+</html>
